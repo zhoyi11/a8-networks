1513e3ef5285f7a63cac3f1fb27eb8e60d475c5d
Joye Zhou <zhoyi@unc.edu>
2024-10-22 21:28:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,9 @@ def make_http_request(ip, port):
 # (This variable is defined at the top of this file if you want to play with it)
 #
 # Hint: This is meant for you to use Google to figure out.
-
+ip_address = socket.gethostbyname(hostname)
+response = make_http_request(ip_address, 80)
+print(response)
 # Next, call make_http_request with the correct parameters and then print the response
 # This response should look a lot like the in-class example of "curl", and start with
 # "HTTP/1.1 200 OK". Rememeber, we're making an HTTP request, so from that you should
